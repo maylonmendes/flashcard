b3da680bc548f78da457eb104c1493035dc9a63e
maylonmendes <141340388+maylonmendes@users.noreply.github.com>
2024-11-18 09:21:09 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -12,14 +12,14 @@ criaCartao(
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'O que é Criptomoeda?',
+    'Uma moeda virtual'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Quizz',
+    'O Pelé tem quantas copas do mundo?',
+    'Ele tem 3'
 )
 
 criaCartao(
